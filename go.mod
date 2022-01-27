6d030ceab7eeb1b1bcdb96e091fa2c5ebc5330e9
Shashank Sinha <shashsinboy@gmail.com>
2022-01-28 00:18:15 +0530

    mongo: CRU operations added

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.17
 
 require (
 	github.com/hashicorp/go-hclog v1.1.0
+	github.com/stretchr/testify v1.7.0
 	go.mongodb.org/mongo-driver v1.8.2
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
 
 require (
@@ -17,8 +19,6 @@ require (
 	github.com/mattn/go-isatty v0.0.10 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
@@ -27,5 +27,6 @@ require (
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
 	golang.org/x/text v0.3.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/go-hclog"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Trainer struct {
@@ -47,12 +48,25 @@ func main() {
 	l := hclog.New(nil)
 	cfg := parseFlags()
 
-	_, err := connectToMongoDB(l, cfg)
+	client, err := connectToMongoDB(l, cfg)
 	if err != nil {
 		l.Error("Cannot continue ahead: exiting")
 		os.Exit(1)
 	}
 
+	collection := client.Database("test").Collection("trainers")
+
+	ash := Trainer{"Ash", 10, "Pallet Town"}
+	misty := Trainer{"Misty", 10, "Cerulean City"}
+	brock := Trainer{"Brock", 15, "Pewter City"}
+
+	insertInCollection(l, collection, ash)
+	insertInCollection(l, collection, misty)
+	insertInCollection(l, collection, brock)
+	updateInCollection(l, collection)
+	findInCollection(l, collection)
+
+	disconnectFromMongoDB(l, client)
 }
 
 // createDSN generates a DSN for mongoDB
@@ -67,6 +81,7 @@ func createDSN(host, username, password string, port int) string {
 	return dsn
 }
 
+// connectToMongoDB uses given params to create a MongoDB client
 func connectToMongoDB(l hclog.Logger, params *Config) (*mongo.Client, error) {
 	dsn := createDSN(params.host, params.username, params.password, params.port)
 
@@ -93,4 +108,62 @@ func connectToMongoDB(l hclog.Logger, params *Config) (*mongo.Client, error) {
 	l.Info("Connected to MongoDB!")
 
 	return client, nil
+}
+
+// disconnectFromMongoDB will disconnect given MongoDB client
+func disconnectFromMongoDB(l hclog.Logger, client *mongo.Client) {
+
+	err := client.Disconnect(context.TODO())
+	if err != nil {
+		l.Error("Error while disconnecting from MongoDB", "error", err)
+	}
+
+	l.Info("Connection to MongoDB closed")
+}
+
+// insertInCollection inserts given document in given collection
+func insertInCollection(l hclog.Logger, col *mongo.Collection, doc interface{}) error {
+
+	insertResult, err := col.InsertOne(context.TODO(), doc)
+	if err != nil {
+		l.Error("Unable to insert document", "error", err)
+		return err
+	}
+
+	l.Info("Inserted a single document: ", "ID", insertResult.InsertedID)
+	return nil
+
+}
+
+func updateInCollection(l hclog.Logger, col *mongo.Collection) error {
+	filter := bson.M{"name": "Ash"}
+
+	update := bson.M{
+		"$inc": bson.M{
+			"age": 1,
+		},
+	}
+
+	updateResult, err := col.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		l.Error("Unable to update document", "error", err)
+		return err
+	}
+
+	l.Info("Updated documents", "Matched documents", updateResult.MatchedCount, "Updated documents", updateResult.ModifiedCount)
+	return nil
+}
+
+func findInCollection(l hclog.Logger, col *mongo.Collection) error {
+	var result Trainer
+	filter := bson.M{"name": "Ash"}
+
+	err := col.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		l.Error("Unable to find document", "error", err)
+		return err
+	}
+
+	l.Info("Found document", "Document", result)
+	return nil
 }
